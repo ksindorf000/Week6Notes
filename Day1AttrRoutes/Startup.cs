6fde43d09a26814fb905f11f50d65635a56bd878
ksindorf000 <ksindorf000@gmail.com>
2017-02-13 09:51:13 -0500

    Add project files.

---
+++ Day1AttrRoutes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Day1AttrRoutes.Startup))]
+namespace Day1AttrRoutes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
