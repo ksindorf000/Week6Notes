12068010c3a43af145133bcad86619e20b6e417c
ksindorf000 <ksindorf000@gmail.com>
2017-02-13 14:02:46 -0500

    Basis for url hashing assignment

--- Day1Hash/Program.cs
+++ Day1Hash/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Day1Hash
@@ -12,21 +13,64 @@ namespace Day1Hash
     {
         static void Main(string[] args)
         {
-            //Step 1: Take User Input
-            var uInput = Console.ReadLine();
+            //Get hashed key from user input
+            var myInput = Console.ReadLine();
+            string hashKey = Encrypt256(myInput);
 
-            //Step 2: Convert to Stream
-            byte[] byteData = Encoding.UTF8.GetBytes(uInput);
-            Stream inputStream = new MemoryStream(); 
+            Console.WriteLine(hashKey);
 
-            //Step 3: Encrypt input and output results
-            using (SHA256 shaHash = new SHA256Managed())
+            int start = 0;
+            int lineNum = 0;
+            bool invalid = true;
+
+            //If first three numbers are out of bounds,
+            //get next three numbers
+            while (invalid) 
             {
-                var result = shaHash.ComputeHash(inputStream);
-                var output = BitConverter.ToString(result);
+                lineNum = int.Parse(hashKey.Substring(start, 3));
+                invalid = lineNum > File.ReadLines(@"..\..\TheSonnets.txt").Count() ? true : false;
+                start++;
+            }
 
-                Console.WriteLine(output.Replace("-","").Substring(0, 8));
-            }                        
+            Console.WriteLine(lineNum);
+            //Capitalize first letter of each word
+            string line = File.ReadLines(@"..\..\TheSonnets.txt").Skip(lineNum).Take(1).First();
+            //http://stackoverflow.com/questions/1943273/convert-all-first-letter-to-upper-case-rest-lower-for-each-word
+            line = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(line.ToLower());
+            Console.WriteLine(line);
+
+            //Rearrange words in line (alphabetize) and strip " "
+            List<string> sList = new List<string>(
+                line.Split(new string[] { " " }, StringSplitOptions.None));
+            sList = sList.OrderByDescending(w => w).ToList();
+            string results = string.Join("", sList.ToArray());
+
+            //Output results
+            Console.WriteLine(results);
+        }
+
+        /*****************************************
+         * Encrypt256()
+         * Hashing code written by Joel in class
+         *****************************************/
+        static string Encrypt256(string input)
+        {
+            string output;
+            byte[] byteData = Encoding.ASCII.GetBytes(input);
+            Stream inputStream = new MemoryStream(byteData);
+
+            using (SHA256 shaM = new SHA256Managed())
+            {
+                var result = shaM.ComputeHash(inputStream);
+                output = BitConverter.ToString(result);
+            }
+
+            //Strip special characters
+            output = output.Replace("-", "");
+            //Strip letters
+            output = Regex.Replace(output, "[A-Za-z ]", "");
+
+            return output;
         }
     }
 }
